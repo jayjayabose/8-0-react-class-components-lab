b74e6445542048ff2d825b751dba5dbc79fb7953
Jay Jayabose <jayabose@gmail.com>
2022-05-31 06:50:45 -0400

    tests passed

--- src/Ad.js
+++ src/Ad.js
@@ -1,6 +1,26 @@
 import React from "react";
 import "./Ad.css";
 
+class Ad extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    const index = Math.floor(Math.random() * adInfo.length);
+    const ad = adInfo[index];
+
+    return(
+      <div className="Ad" data-testid="ad">
+      <img src={ad.src} alt="cool ad" />
+      <p data-testid="ad-text">{ad.text}</p>
+      </div>
+    )
+  }
+
+}
+
+
 export const adInfo = [
   {
     src: "https://placeimg.com/200/200/arch",
@@ -15,7 +35,7 @@ export const adInfo = [
     text: "Celebs: Where are they now??",
   },
 ];
-
+/*
 const Ad = () => {
   const index = Math.floor(Math.random() * adInfo.length);
   const ad = adInfo[index];
@@ -27,5 +47,5 @@ const Ad = () => {
     </div>
   );
 };
-
+*/
 export default Ad;
